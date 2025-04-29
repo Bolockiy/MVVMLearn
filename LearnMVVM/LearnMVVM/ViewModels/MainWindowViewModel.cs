e8e1c3bb5552dd3fbd628c13d84b41ca38d80242
Болоцкий Дмитрий Сергеевич <dbolotskiy@phoenixit.ru>
2025-04-29 14:49:44 +0300

    добавлен биндинг

---
+++ LearnMVVM/LearnMVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using LearnMVVM.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnMVVM.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _title = "Тестовое mvvm";
+        public string Title { 
+            get => _title;
+            //if(value == _title) return;
+            //_title = value;
+            //    onPropertyChanged();
+            //    Set(ref _title, value);
+            set => Set(ref _title, value);
+        }
+    }
+}
