ca0c2c70a9134d55642e49cfd68c2ab0f4694fd7
Болоцкий Дмитрий Сергеевич <dbolotskiy@phoenixit.ru>
2025-04-29 12:52:33 +0300

    Базовый класс модели представления

---
+++ LearnMVVM/LearnMVVM/ViewModels/Base/ViewModelcs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnMVVM.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    { 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void onPropertyChanged([CallerMemberName] string ProperyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(ProperyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyrName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            onPropertyChanged(PropertyrName);
+            return true;
+        }
+    }
+}
